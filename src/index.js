bed5879ac18d5cea50f3004af8f418a1de6ab78c
MonaBurger <mona@software-lab.de>
2024-01-02 10:37:45 +0100

    addedjs to make select work

--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
 function updateTime() {
   let losAngeles = document.querySelector("#los-angeles");
-  let losAngelesDate = losAngeles.querySelector(".date");
-  let losAngelesTime = losAngeles.querySelector(".time");
-  let losAngelesCurrentTime = moment().tz("America/Los_Angeles");
-  losAngelesDate.innerHTML = losAngelesCurrentTime.format("MMMM Do YYYY");
+  if (losAngeles) {
+    let losAngelesDate = losAngeles.querySelector(".date");
+    let losAngelesTime = losAngeles.querySelector(".time");
+    let losAngelesCurrentTime = moment().tz("America/Los_Angeles");
+    losAngelesDate.innerHTML = losAngelesCurrentTime.format("MMMM Do YYYY");
 
-  losAngelesTime.innerHTML = losAngelesCurrentTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesTime.innerHTML = losAngelesCurrentTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let tokyo = document.querySelector("#tokyo");
-  let tokyoDate = tokyo.querySelector(".date");
-  let tokyoTime = tokyo.querySelector(".time");
-  let tokyoCurrentTime = moment().tz("Asia/Tokyo");
-  tokyoDate.innerHTML = tokyoCurrentTime.format("MMMM Do YYYY");
+  if (tokyo) {
+    let tokyoDate = tokyo.querySelector(".date");
+    let tokyoTime = tokyo.querySelector(".time");
+    let tokyoCurrentTime = moment().tz("Asia/Tokyo");
+    tokyoDate.innerHTML = tokyoCurrentTime.format("MMMM Do YYYY");
 
-  tokyoTime.innerHTML = tokyoCurrentTime.format("h:mm:ss [<small>]A[</small>]");
+    tokyoTime.innerHTML = tokyoCurrentTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
+let citySelectorElement = document.querySelector("#city");
+citySelectorElement.addEventListener("change", updateCity);
